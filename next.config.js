c0cfc45363ed08d85a2e1d973eb2a992ccd4a08e
Mert Yildirim <mertsyildirim@gmail.com>
2025-04-07 18:54:18 +0300

    Statik site dağıtımı için yapılandırma güncellendi

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
   env: {
     GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-  },
-  images: {
-    unoptimized: true,
-    domains: ['maps.googleapis.com']
-  },
-  experimental: {
-    serverActions: true
   }
 };
 
