f44e3ab9d03f014a169f735a1a320597e3868e22
Mert Yildirim <mertsyildirim@gmail.com>
2025-04-08 09:57:17 +0300

    Ana sayfa yönlendirmesi düzeltildi

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/',
+        destination: '/portal/login',
+      },
+      {
         source: '/portal',
         destination: '/portal/login',
       },
